4c60501101f22c1c1dcb7e8669ba13e3bd247355
Venky Shankar <vshankar@redhat.com>
2019-08-05 05:32:28 -0400

    mds: record metrics from all MDSs in MDS rank 0
    `MetricAggregator` class aggregates metrics from all active ranks
    and places metrics appropriately as defined by user queries.
    
    This is implemented as a separate dispatcher since metric update
    messages from active MDSs are frequent so as to avoid messages
    getting stuck in MDSRank queue (suggested by Patrick).
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/mds/MDSRank.h
+++ src/mds/MDSRank.h
@@ -118,6 +118,7 @@ class SnapClient;
 class MDSTableServer;
 class MDSTableClient;
 class Messenger;
+class MetricAggregator;
 class Objecter;
 class MonClient;
 class MgrClient;
@@ -531,6 +532,8 @@ class MDSRank {
     // because its init/shutdown happens at the top level.
     PurgeQueue purge_queue;
 
+    std::unique_ptr<MetricAggregator> metric_aggregator;
+
     list<cref_t<Message>> waiting_for_nolaggy;
     MDSContext::que finished_queue;
     // Dispatch, retry, queues
@@ -578,6 +581,10 @@ private:
     void get_task_status(std::map<std::string, std::string> *status);
     void schedule_update_timer_task();
     void send_task_status();
+
+    bool is_rank0() const {
+      return whoami == (mds_rank_t)0;
+    }
 
     mono_time starttime = mono_clock::zero();
 };
