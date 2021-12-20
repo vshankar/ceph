41f1947f180dfea11077f52c17644b764a289cb9
Xiubo Li <xiubli@redhat.com>
2021-12-20 10:21:50 +0800

    client: force a request to update the perms
    If the caller perms is different with the current dirtier's, we
    can still cache the attributes locally if possible, but will
    mark the ctime needs to be updated forcely and the request will
    implicitly flush the dirty caps for us.
    
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -7483,9 +7483,7 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
      * case later, we can build a more complex pipelined cap writeback
      * infrastructure...
      */
-    if (!mask)
-      mask |= CEPH_SETATTR_CTIME;
-    goto force_request;
+    mask |= CEPH_SETATTR_CTIME;
   }
 
   if (!mask) {
@@ -7685,7 +7683,6 @@ int Client::_do_setattr(Inode *in, struct ceph_statx *stx, int mask,
     return 0;
   }
 
-force_request:
   MetaRequest *req = new MetaRequest(CEPH_MDS_OP_SETATTR);
 
   filepath path;
