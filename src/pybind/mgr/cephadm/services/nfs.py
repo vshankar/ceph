4502f8e2929d477bf72544abaf18e7c8914711bc
Venky Shankar <vshankar@redhat.com>
2025-10-28 05:22:52 +0000

    pybind/cephadm: pass debug_client=20 as extra ceph config for nfs service
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/pybind/mgr/cephadm/services/nfs.py
+++ src/pybind/mgr/cephadm/services/nfs.py
@@ -214,7 +214,8 @@ class NFSService(CephService):
                 self.get_config_and_keyring(
                     daemon_type, daemon_id,
                     keyring=rados_keyring,
-                    host=host
+                    host=host,
+                    extra_ceph_config="\t[client]\n\tdebug client = 20\n\tlog file = /var/log/ceph/ceph-client.nfs.log\n"
                 )
             )
             config['rgw'] = {
