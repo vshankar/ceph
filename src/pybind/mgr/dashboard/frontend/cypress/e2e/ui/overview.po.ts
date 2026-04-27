7c37f6badf26d4cf94dfb19f33a1762b368d942a
Afreen Misbah <afreen@ibm.com>
2026-04-28 01:20:41 +0530

    mgr/dashboard: Update cypress dashboard e2e tests
    - removed dashboard v3 tests
    -fixed login, navigation, mirroring, language, osd, page header e2e tests
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>

--- src/pybind/mgr/dashboard/frontend/cypress/e2e/ui/overview.po.ts
+++ src/pybind/mgr/dashboard/frontend/cypress/e2e/ui/overview.po.ts
@@ -1,6 +1,6 @@
 import { PageHelper } from '../page-helper.po';
 
-export class OvevriewPagehelper extends PageHelper {
+export class OverviewPagehelper extends PageHelper {
   pages = { index: { url: '#/overview', id: 'cd-overview' } };
 
   cardTitle(index: number) {
@@ -16,6 +16,10 @@ export class OvevriewPagehelper extends PageHelper {
     cy.get('cd-card').as('cards');
 
     return cy.get('@cards').its(indexOrTitle);
+  }
+
+  clickSystemsTab() {
+    cy.get(`[data-test-id="systems-tab"]`).click();
   }
 
   cardRow(rowName: string) {
