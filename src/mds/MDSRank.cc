b6487b8520caab569fcdae392b4b57db80085665
Venky Shankar <vshankar@redhat.com>
2026-04-30 11:35:05 +0530

    mds: remove duplicate context completion calls
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -3108,13 +3108,7 @@ void MDSRankDispatcher::handle_asok_command(
   } else if (command == "dump stray") {
     dout(10) << "dump_stray start" <<  dendl;
     // the context is a wrapper for formatter to be used while scanning stray dir
-    auto context = std::make_unique<MDCache::C_MDS_DumpStrayDirCtx>(mdcache, f,
-     [this,on_finish](int r) {
-      // completion callback, will be called when scan is done
-      dout(10) << "dump_stray done" <<  dendl;
-      bufferlist bl;
-      on_finish(r, "", bl);
-    });
+    auto context = std::make_unique<MDCache::C_MDS_DumpStrayDirCtx>(mdcache, f, on_finish);
     std::lock_guard l(mds_lock);
     r = mdcache->stray_status(std::move(context));
     // since the scanning op can be async, we want to know it, for better semantics
